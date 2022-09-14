65edee8a56e30135253086b090e21f1eb51453ed
Yasin Hoca <myasinozsaglam@gmail.com>
2022-09-14 12:26:24 +0300

    Add files via upload

---
+++ contracts/01_giris.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.7;
+//pragma solidity >=0.8.7 <0.9.5
+
+contract ilk{
+    uint public sayi1=5;
+    uint public sayi2=8;
+    uint public toplam=sayi1+sayi2;
+}
